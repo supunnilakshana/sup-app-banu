27d029f711f1cda847e6ea70bf775df326af3fe1
dinithiappri <158431409+dinithiappri@users.noreply.github.com>
2024-04-03 23:23:10 +0530

    feat : added item service

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,38 +1,48 @@
 // 'use client'
 
-import { CreateItemTypeDto, CreateMeasurementDto, ItemTypeDto } from '@/dto';
-import ItemTypeService from '@/services/item_type_service';
-import React from 'react';
+import {
+  CreateItemDto,
+  CreateItemTypeDto,
+  CreateMeasurementDto,
+  ItemTypeDto,
+} from "@/dto";
+import ItemService from "@/services/item_service";
+import ItemTypeService from "@/services/item_type_service";
+import React from "react";
 
-const itemTypeService : ItemTypeService = new ItemTypeService(); 
+const itemTypeService: ItemTypeService = new ItemTypeService();
+const itemsService: ItemService = new ItemService();
 
+const Home: React.FC = async () => {
+  const list_item: ItemTypeDto[] = await itemTypeService.getItemTypes();
 
-const Home: React.FC =async () => {
-    
-    const list_item:ItemTypeDto[] = await itemTypeService.getItemTypes();
+  const itemType: CreateItemTypeDto = {
+    name: "test",
+  };
 
+  // const item: CreateItemDto= {
+  //     name: "biscuit",
+  //     image : "",
+  //     type_id:
 
-    const itemType: CreateItemTypeDto = {
-        name: 'test',
-    };
-   
+  // }
 
-    const sendData =async () => {
-      itemTypeService.createItemType(itemType);
-    };
+  const sendData = async () => {
+    itemTypeService.createItemType(itemType);
+  };
 
-    return (
-        <div>
-            <h1>Welcome to the Home Page</h1>
-            {/* Add your content here */}
-            {/* <button onClick={sendData}>Send Data</button> */}
-            <ul>
-                {list_item.map((item) => (
-                    <li key={item.id}>{item.name}</li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      {/* Add your content here */}
+      {/* <button onClick={sendData}>Send Data</button> */}
+      <ul>
+        {list_item.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
--- src/services/item_service.ts
+++ src/services/item_service.ts
@@ -1,5 +1,49 @@
+import { CreateItemDto, ItemDto, UpdateItemDto } from "@/dto";
+import supabase from "@/utils/supabase-client";
+
 class ItemService {
   constructor() {}
+
+  async createItem(item: CreateItemDto): Promise<void> {
+    try {
+      await supabase.from("items").insert(item);
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async getItem(): Promise<ItemDto[]> {
+    try {
+      const { data, error } = await supabase.from("items").select("*");
+      if (error) {
+        throw error;
+      }
+
+      return data as ItemDto[];
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async updateItem(item: UpdateItemDto, id: number): Promise<void> {
+    try {
+      await supabase.from("items").update(item).eq("id", id);
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async deleteItem(id: number): Promise<void> {
+    try {
+      await supabase.from("items").delete().eq("id", id);
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
 }
 
 export default ItemService;
